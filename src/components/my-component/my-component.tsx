8b5ea94789abd0666cfdc7c8ff8e104fdb231ef1
Muthupriya <muthupriya.s@ai12z.com>
2024-05-30 08:32:27 -0400

    Added the scenario

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, h } from '@stencil/core';
+import { Component, Prop, h , Element} from '@stencil/core';
 import { format } from '../../utils/utils';
 
 @Component({
@@ -7,6 +7,8 @@ import { format } from '../../utils/utils';
   shadow: true,
 })
 export class MyComponent {
+  @Element() el!: HTMLElement
+  isCta: boolean = false;
   /**
    * The first name
    */
@@ -25,8 +27,28 @@ export class MyComponent {
   private getText(): string {
     return format(this.first, this.middle, this.last);
   }
+  componentWillLoad() {
+    const slot = this.el['children'];
+    let slotHasContent = slot && slot.length > 0;
+
+    if(slotHasContent) {
+        for(var i=0;i<slot.length;i++) {
+          if(slot[i].tagName.toLowerCase() === 'cta' || slot[i].getAttribute('slot') === 'cta') {
+            this.isCta = true;
+          }
+        }
+      }
+  }
+
+  defaultTemplate() {
+    return <div>Hello, World! I'm {this.getText()}</div>;
+  }
 
   render() {
-    return <div>Hello, World! I'm {this.getText()}</div>;
+    return (
+      <div>
+        {!this.isCta && this.defaultTemplate()} 
+        {this.isCta && <slot name="cta"/>}
+    </div>)
   }
 }
